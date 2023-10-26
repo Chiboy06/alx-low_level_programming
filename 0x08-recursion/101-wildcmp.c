c66003161c3f2ee5e6baf4e883949fdf2fd703b0
Chinaza <elechukwujosh3@gmail.com>
2023-10-25 19:48:44 -0700

    0x08-recursive

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,41 +1,63 @@
 #include "main.h"
 
-int wildcmp_recursive(char *s1, char *s2, int s1_index, int s2_index)
+int wildmode(char *s1, char *s2);
+
+/**
+ * exactmode - Goes to end of found substring
+ *
+ * @s1: non-wildcarded string we're checking for matches
+ * @s2: wildcarded string we're checking the substring from
+ *
+ * Return: continue wild mode at end of substring if
+ * substring found, 0 otherwise
+ */
+int exactmode(char *s1, char *s2)
 {
-    if (s1[s1_index] == '\0' && s2[s2_index] == '\0')
-    {
-        return 1; /* Both strings are empty, and we've successfully matched them. */
-    }
-
-    if (s2[s2_index] == '*')
-    {
-        while (s2[s2_index + 1] == '*')
-        {
-            s2_index++; /* Skip consecutive '*' characters in s2. */
-        }
-
-        while (s1[s1_index] != '\0')
-        {
-            if (wildcmp_recursive(s1, s2, s1_index, s2_index + 1))
-            {
-                return 1; /* Matched using '*' as an empty string. */
-            }
-            s1_index++;
-        }
-
-        return wildcmp_recursive(s1, s2, s1_index, s2_index + 1); /* Matched using '*' as one or more characters. */
-    }
-
-    if (s1[s1_index] == s2[s2_index] || (s1[s1_index] != '\0' && s2[s2_index] == '?'))
-    {
-        return wildcmp_recursive(s1, s2, s1_index + 1, s2_index + 1); /* Matched, continue comparing. */
-    }
-
-    return 0; /* Characters don't match, not identical. */
+	if (*s2 == '*')
+		return (wildmode(s1, s2 + 1));
+	if (*s1 == 0 && *s2 == 0)
+		return (1);
+	if (*s1 == *s2)
+		return (exactmode(s1 + 1, s2 + 1));
+	return (0);
 }
 
+/**
+ * wildmode - Processes wildcards from s2 and handles backtracking
+ * to match multiple instances of wildcarded substrings
+ *
+ * @s1: string we're trying to match
+ * @s2: wildcarded string containing search pattern
+ *
+ * Return: 1 if valid match, 0 if not
+ */
+int wildmode(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (wildmode(s1, s2 + 1));
+	if (*s2 == 0)
+		return (1);
+	if (*s1 == 0)
+		return (0);
+	if (*s1 != *s2)
+		return (wildmode(s1 + 1, s2));
+	if (!(exactmode(s1 + 1, s2 + 1)))
+		return (wildmode(s1 + 1, s2));
+	return (1);
+}
+
+/**
+ * wildcmp - Compares two strings with * wildcards. Sets initial search mode.
+ *
+ * @s1: first string, does not have *
+ * @s2: second string, has *
+ *
+ * Return: 1 if identical, otherwise 0
+ */
 int wildcmp(char *s1, char *s2)
 {
-    return wildcmp_recursive(s1, s2, 0, 0);
+	if (*s2 == '*')
+		return (wildmode(s1, s2));
+	return (exactmode(s1, s2));
 }
 
