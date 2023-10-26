664357d27a0b587d825fd535b6948ba758949b35
Chinaza <elechukwujosh3@gmail.com>
2023-10-25 19:45:52 -0700

    0x08-recursive

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,36 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * _strlen_recursion - return length of string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * palindrome_finder - finds palindromes
+ *
+ * @s: string to find palindrome of
+ * @l: last offset of string
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int palindrome_finder(char *s, int l)
+{
+	if (l < 1)
+		return (1);
+	if (*s == *(s + l))
+		return (palindrome_finder(s + 1, l - 2));
+	return (0);
+}
+
 /**
  * is_palindrome - returns if string is palindrome or not
  *
@@ -7,23 +38,10 @@
  *
  * Return: 1 if palindrome, 0 otherwise
  */
-int is_palindrome_recursive(char *s, int start, int end)
+int is_palindrome(char *s)
 {
-	if (start >= end)
-	{
-		return (1);
-	}
-	if (s[start] == s[end])
-	{
-		return is_palindrome(s, start + 1, end - 1);
-	}
+	int len = _strlen_recursion(s);
 
-	return (0);
+	return (palindrome_finder(s, len - 1));
 }
 
-int is_palindrome(char *s, int start, int end)
-{
-	int length = strlen(s);
-
-	return is_palindrome_recursive(s, 0, length - 1);
-}
